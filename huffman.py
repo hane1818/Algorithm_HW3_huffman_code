4e243ade9b96c5ea6e68c27593fb578c52c85f1a
Hane Liu <hane0131@gmail.com>
2015-05-04 22:46:55 +0800

    Add functions about setting the parent & children nodes and codes.

--- huffman.py
+++ huffman.py
@@ -8,3 +8,15 @@ class Node:
     def initSet(self, name, weight):
         self.name = name
         self.weight = weight
+
+    def setRoot(self, root):
+        self.root = root
+
+    def setLeft(self, left):
+        self.left = left
+
+    def setRight(self, right):
+        self.right = right
+
+    def addCode(self, code):
+        self.code = code + self.code
