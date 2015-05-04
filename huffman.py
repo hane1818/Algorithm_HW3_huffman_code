f497d51a9736cfac5ececaf1a729a04ad74ea8bd
Hane Liu <hane0131@gmail.com>
2015-05-04 11:10:19 +0800

    Initialize and declare class Node

---
+++ huffman.py
@@ -0,0 +1,10 @@
+class Node:
+
+    def __init__(self):
+        self.name = ''
+        self.weight = 0
+        self.code = ''
+
+    def initSet(self, name, weight):
+        self.name = name
+        self.weight = weight
